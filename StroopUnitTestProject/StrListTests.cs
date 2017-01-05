526b662b8056351b615f8b4bccf7bb45ccfc415a
fabiolamf <fabiola.mfleury@gmail.com>
2017-01-05 15:56:59 -0200

    Created unit test configurations

---
+++ StroopUnitTestProject/StrListTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StroopTest;
+using StroopTest.Models;
+using StroopTest.Views;
+using StroopTest.Controllers;
+
+namespace StroopUnitTestProject
+{
+    [TestClass]
+    public class StrListTests
+    {
+        [TestMethod]
+        public void StrListObjectTest()
+        {
+            List<string> list = new List<string>(new string[] { "element1", "element2", "element3" });
+            StrList strList = new StrList( list, "list");
+            Assert.IsTrue(strList is StrList);
+        }
+    }
+}

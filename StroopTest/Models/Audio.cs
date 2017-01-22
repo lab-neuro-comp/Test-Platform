9602a238986b818e0f4466e8d364b6c309588c7b
fabiolamf <fabiola.mfleury@gmail.com>
2017-01-22 20:15:08 -0200

    Updated new audio class

---
+++ StroopTest/Models/Audio.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace StroopTest.Models
+{
+    class Audio
+    {
+
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string MciComando, string MciRetorno, int MciRetornoLeng, int CallBack);
+
+        public bool startRecording()
+        {
+            mciSendString("open new type waveaudio alias audio", null, 0, 0);
+            mciSendString("record audio", null, 0, 0);
+            return true;
+        }
+        public bool pauseRecording()
+        {
+            mciSendString("pause audio", null, 0, 0);
+            return true;
+        }
+        public bool saveRecording(string path)
+        {
+            mciSendString("pause audio", null, 0, 0);
+            mciSendString("save audio " + path, null, 0, 0);
+            mciSendString("close audio", null, 0, 0);
+            return true;
+        }
+        public void playAudio(string file)
+        {
+            mciSendString("play " + file, null, 0, 0);
+        }
+    }
+}
--- StroopTest/Views/FormNewAudio.cs
+++ StroopTest/Views/FormNewAudio.cs
@@ -1,19 +1,14 @@
 ﻿using System;
-using System.Media;
 using System.Windows.Forms;
 using StroopTest.Models;
 using StroopTest.Views;
-using StroopTest.Controllers;
-using NAudio.Wave;
-using System.Runtime.InteropServices;
 
 namespace StroopTest
 {
     public partial class FormNewAudio : Form
     {
         private string instructionsText = HelpData.NewAudioInstructions;
-        [DllImport("winmm.dll")]
-        private static extern int mciSendString(string MciComando, string MciRetorno, int MciRetornoLeng, int CallBack);
+        Audio audioRecorder = new Audio();
         private string file = "";
         public FormNewAudio()
         {
@@ -25,36 +20,32 @@ namespace StroopTest
         private void recordingButton_Click(object sender, EventArgs e)
         {
             recordingLabel.Visible = true;
-            mciSendString("open new type waveaudio alias Som", null, 0, 0);
-            mciSendString("record Som", null, 0, 0);
+            audioRecorder.startRecording();
         }
         
         private void stopRecordingButton_Click(object sender, EventArgs e)
         {
             recordingLabel.Visible = false;
-            mciSendString("pause Som", null, 0, 0);
             SaveFileDialog save = new SaveFileDialog();
             save.Filter = "WAVE|*.wav";
 
             if (save.ShowDialog() == DialogResult.OK) // this is where it needs to be altered
             {
-                mciSendString("save Som " + save.FileName, null, 0, 0);
-                mciSendString("close Som", null, 0, 0);
+                audioRecorder.saveRecording(save.FileName);
             }
         }
         private void btnPlay_Click(object sender, EventArgs e)
         {
            OpenFileDialog open = new OpenFileDialog();
            open.Filter = "Wave|*.wav";
-           if (open.ShowDialog() == DialogResult.OK) { file = open.FileName; }            
-            mciSendString("play " + file, null, 0, 0);
+           if (open.ShowDialog() == DialogResult.OK) { file = open.FileName; }
+            audioRecorder.playAudio(file);           
         }
 
         private void helpButton_Click(object sender, EventArgs e)
         {
             FormInstructions infoBox = new FormInstructions(instructionsText);
-            try { infoBox.Show(); }
-            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            infoBox.Show();
         }
 
         private void closeButton_Click(object sender, EventArgs e)

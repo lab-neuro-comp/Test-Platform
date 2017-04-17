a983a89f1c364dd39b9c3288543de8f1f96d2559
fabiolamf <fabiola.mfleury@gmail.com>
2017-04-17 11:52:23 -0300

    Creating reaction test exposition

---
+++ StroopTest/Models/ReactionTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPlatform.Models
+{
+    class ReactionTest
+    {
+        // todos os dados de horario devem guardar hora, minuto e segundo
+        ReactionProgram programInUse = new ReactionProgram();
+        string participantName;
+        DateTime initialTime;
+        List<DateTime> estimuluAppearedTime;
+        List<DateTime> hitTime;
+
+        public void setProgramInUse(string path, string prgName)
+        {
+            if (File.Exists(path + "/prg/" + prgName + ".prg"))
+            {
+                programInUse.readProgramFile(path + "/prg/");
+            }
+            else
+            {
+                throw new Exception("Arquivo programa: " + programInUse.ProgramName + ".prg" +
+                                    "\nnão foi encontrado no local:\n" + Path.GetDirectoryName(path + "/prg/"));
+            }
+        }
+
+
+    }
+}
--- StroopTest/Views/FormReactExposition.cs
+++ StroopTest/Views/FormReactExposition.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using TestPlatform.Models;
 
 namespace TestPlatform.Views
 {
     public partial class FormReactExposition : Form
     {
-        public FormReactExposition()
+        ReactionProgram programInUse = new ReactionProgram();
+        private static float elapsedTime;               
+        private string path;                           
+        private List<string> outputContent;            
+        private string outputDataPath;                
+        private string hour = DateTime.Now.Hour.ToString("00");
+        private string minutes = DateTime.Now.Minute.ToString("00");
+        private string seconds = DateTime.Now.Second.ToString("00");
+        private string startTime;
+        private string outputFile;
+
+
+        public FormReactExposition(string defaultPath)
         {
+            path = defaultPath;
+            outputDataPath = defaultPath + "/data";
+            startTime = hour + "_" + minutes + "_" + seconds;
             InitializeComponent();
         }
     }

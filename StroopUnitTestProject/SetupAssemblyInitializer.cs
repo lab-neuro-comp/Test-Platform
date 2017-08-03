b559ce0597ba96c207728fbd8aa605a2acd6cbf9
fabiolamf <fabiola.mfleury@gmail.com>
2017-08-03 12:23:29 -0300

    Creating StrList unit tests

---
+++ StroopUnitTestProject/SetupAssemblyInitializer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using TestPlatform.Views;
+
+namespace StroopUnitTestProject
+{
+    [TestClass]
+    public class SetupAssemblyInitializer
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            Global.defaultPath = (Path.GetDirectoryName(path));
+            Global.testFilesPath = Global.defaultPath + Global.testFilesPath;
+            if (!Directory.Exists(Global.testFilesPath))
+            {
+                Directory.CreateDirectory(Global.testFilesPath);
+            }
+
+            Global.stroopTestFilesPath = Global.testFilesPath + Global.stroopTestFilesPath;
+            if (!Directory.Exists(Global.stroopTestFilesPath))
+                Directory.CreateDirectory(Global.stroopTestFilesPath);
+            if (!Directory.Exists(Global.stroopTestFilesPath + Global.programFolderName))
+                Directory.CreateDirectory(Global.stroopTestFilesPath + Global.programFolderName);
+            if (!Directory.Exists(Global.stroopTestFilesPath + Global.resultsFolderName))
+                Directory.CreateDirectory(Global.stroopTestFilesPath + Global.resultsFolderName);
+
+            Global.reactionTestFilesPath = Global.testFilesPath + Global.reactionTestFilesPath;
+            if (!Directory.Exists(Global.reactionTestFilesPath))
+                Directory.CreateDirectory(Global.reactionTestFilesPath);
+            if (!Directory.Exists(Global.reactionTestFilesPath + Global.programFolderName))
+                Directory.CreateDirectory(Global.reactionTestFilesPath + Global.programFolderName);
+            if (!Directory.Exists(Global.reactionTestFilesPath + Global.resultsFolderName))
+                Directory.CreateDirectory(Global.reactionTestFilesPath + Global.resultsFolderName);
+
+            /* creating directories related to Experiment in case they don't already exists*/
+            Global.experimentTestFilesPath = Global.testFilesPath + Global.experimentTestFilesPath;
+            if (!Directory.Exists(Global.experimentTestFilesPath))
+                Directory.CreateDirectory(Global.experimentTestFilesPath);
+            if (!Directory.Exists(Global.experimentTestFilesPath + Global.programFolderName))
+                Directory.CreateDirectory(Global.experimentTestFilesPath + Global.programFolderName);
+            if (!Directory.Exists(Global.experimentTestFilesPath + Global.resultsFolderName))
+                Directory.CreateDirectory(Global.experimentTestFilesPath + Global.resultsFolderName);
+
+            /* creating Lists folder*/
+            if (!Directory.Exists(Global.testFilesPath + Global.listFolderName))
+            {
+                Directory.CreateDirectory(Global.testFilesPath + Global.listFolderName);
+            }
+
+            if (!Directory.Exists(Global.defaultPath + Global.backupFolderName))
+                Directory.CreateDirectory(Global.defaultPath + Global.backupFolderName);
+        }
+    }
+}
--- StroopUnitTestProject/StrListTests.cs
+++ StroopUnitTestProject/StrListTests.cs
@@ -1,24 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Web;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using StroopTest;
-using StroopTest.Models;
-using StroopTest.Views;
-using StroopTest.Controllers;
+using TestPlatform.Models;
+using System.IO;
+using TestPlatform.Views;
+using System;
+using System.Windows.Documents;
 
 namespace StroopUnitTestProject
 {
     [TestClass]
     public class StrListTests
     {
+        string listsPath;
+        string[] wordList = new string[] { "amarelo", "azul", "verde", "vermelho" };
+        string[] colorList = new string[] { "#F8E000", "#007BB7", "#7EC845", "#D01C1F" };
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            listsPath = Global.testFilesPath + Global.listFolderName;
+            StrList.writeDefaultColorsList(listsPath);
+            StrList.writeDefaultWordsList(listsPath);
+        }
+
         [TestMethod]
         public void StrListObjectTest()
         {
             List<string> list = new List<string>(new string[] { "element1", "element2", "element3" });
-            StrList strList = new StrList( list, "list");
+            StrList strList = new StrList( list, "list", "_word");
             Assert.IsTrue(strList is StrList);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ReadInexistentList()
+        {
+            StrList.readListFile(null);
+        }
+
+        [TestMethod]
+        public void WordListExists()
+        {
+            List<string> list = new List<string>(wordList);
+            StrList strList = new StrList(list,"padrao", "_word");
+            Assert.IsTrue(strList is StrList);
+        }
+
+        [TestMethod]
+        public void ReadStandardWordListTest()
+        {
+            string[] expected = new string[] { "amarelo", "azul", "verde", "vermelho" };
+            string[] actual = StrList.readListFile(listsPath +"padrao_words.lst");
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+        }
+
+        [TestMethod]
+        public void ReadStandardColorListTest()
+        {
+            string[] expected = new string[] {"#F8E000", "#007BB7", "#7EC845", "#D01C1F"};
+            string[] actual = StrList.readListFile(listsPath + "padrao_color.lst");
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
         }
     }
 }
